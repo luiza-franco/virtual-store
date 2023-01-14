38b0e3a714499ed00c3976b32d32639e0447f95f
Luiza Bade Franco <luizarbfranco@gmail.com>
2023-01-14 16:33:25 -0300

    route creation

--- index.tsx
+++ index.tsx
@@ -3,3 +3,24 @@ const express = require('express')
 const app = express()
 
 app.listen('3000')
+
+
+  app.get('/products', function (req, res) {
+    res.send('Listar os produtos');
+  });
+
+  app.get('/product', function (req, res) {
+    res.send('Listar o produto');
+  });
+   
+  app.post('/product', function (req, res) {
+    res.send('Inserir o produto');
+  });
+   
+  app.put('/product/:id', function (req, res) {
+    res.send(`Editar o produto ${req.params.id}`);
+  });
+   
+  app.delete('/product/:id', function (req, res) {
+    res.send(`Excluir o produto ${req.params.id}`);
+  });
